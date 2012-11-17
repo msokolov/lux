1e60fb29de15c2e24da0ad7507750d110f9d7e84
sokolov <sokolov@d2cdb213-60f8-4f1b-b4b2-f6b941818144>
2012-11-17 18:10:01 +0000

    js support for autocomplete for demo application
    git-svn-id: http://falutin.net/svn/falutin/lux@1216 d2cdb213-60f8-4f1b-b4b2-f6b941818144

---
+++ src/main/webapp/js/getQNames.js
@@ -0,0 +1,16 @@
+﻿/*jslint  browser: true, white: true, plusplus: true */
+/*global $: true */
+
+$(function () {
+    'use strict';
+    $.ajax({
+        url: 'getQNames.xqy',
+        dataType: 'json'
+    }).done(function (data) {
+        var status = $('#selection');
+        $('#query').autocomplete({
+            lookup: data,
+            serviceUrl:'getQNames.xqy'
+        });
+    });
+});
