7dba861599317039a7f363be3195bd5fe3edf12f
sokolov <sokolov@d2cdb213-60f8-4f1b-b4b2-f6b941818144>
2012-11-19 23:19:37 +0000

    override search execution so we can ignore start and len parameters which solr typically respects
    git-svn-id: http://falutin.net/svn/falutin/lux@1236 d2cdb213-60f8-4f1b-b4b2-f6b941818144

--- src/main/java/lux/solr/AppServer.java
+++ src/main/java/lux/solr/AppServer.java
@@ -4,14 +4,20 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import lux.api.QueryStats;
+import lux.search.LuxSearcher;
+
 import net.sf.saxon.s9api.XdmItem;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.handler.component.ResponseBuilder;
 import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.search.SolrIndexSearcher;
 
 public class AppServer extends XQueryComponent {
 
@@ -42,6 +48,35 @@ public class AppServer extends XQueryComponent {
             }
         }
     }
+    
+    /**
+     * ignores start and len query parameters
+     */
+    @Override
+    public void process(ResponseBuilder rb) throws IOException {
+
+        SolrQueryRequest req = rb.req;
+        SolrQueryResponse rsp = rb.rsp;
+        SolrParams params = req.getParams();
+        if (!params.getBool(COMPONENT_NAME, true)) {
+          return;
+        }
+        SolrIndexSearcher searcher = req.getSearcher();
+        SolrIndexSearcher.QueryResult result = new SolrIndexSearcher.QueryResult();
+        int start = 1;
+        long timeAllowed = (long)params.getInt( CommonParams.TIME_ALLOWED, -1 );
+        int len = -1;
+        // multiple shards not implemented
+        evaluator.setSearcher(new LuxSearcher(searcher));
+        evaluator.setQueryStats(new QueryStats());
+        String query = rb.getQueryString();
+        if (StringUtils.isBlank(query)) {
+            rsp.add("xpath-error", "query was blank");
+        } else {
+            evaluateQuery(rb, rsp, result, start, timeAllowed, len, query);
+        }
+    }
+
     
     protected void addResult(NamedList<Object> xpathResults, XdmItem item) {
         xpathResults.add("result", item);
