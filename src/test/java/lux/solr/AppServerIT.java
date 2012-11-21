9c3555611645706b5a627d11638f28f69a1269ca
sokolov <sokolov@d2cdb213-60f8-4f1b-b4b2-f6b941818144>
2012-11-21 03:01:58 +0000

    updated tests to reflect that we now return 500 status on error added test for plain text content updated test to reflect HTML serialization
    git-svn-id: http://falutin.net/svn/falutin/lux@1247 d2cdb213-60f8-4f1b-b4b2-f6b941818144

--- src/test/java/lux/solr/AppServerIT.java
+++ src/test/java/lux/solr/AppServerIT.java
@@ -1,12 +1,14 @@
 package lux.solr;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.meterware.httpunit.WebClient;
 import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebResponse;
 
 /**
  * basic test to make sure the app server is functioning - the app server can be passed a query by 
@@ -22,32 +24,48 @@ public class AppServerIT {
     
     @Test
     public void testAppServer () throws Exception {
-        String path = (APP_SERVER_PATH + "/src/test/resources/lux/test1.xqy");
+        String path = (APP_SERVER_PATH + "/test/test1.xqy");
         String response = httpclient.getResponse(path).getText();
         assertEquals ("<test>1 + 1 = 2</test>", response);
     }
     
     @Test
     public void testSyntaxError () throws Exception {
-        String path = (APP_SERVER_PATH + "/src/test/resources/lux/undeclared.xqy");
-        String response = httpclient.getResponse(path).getText();
-        assertEquals ("<html><head><title>ERROR</title></head>" +
-        		"<body>ERROR: Variable $undeclared has not been declared</body></html>", response);
+        String path = (APP_SERVER_PATH + "/test/undeclared.xqy");
+        WebResponse httpResponse = httpclient.getResponse(path);
+        assertEquals (500, httpResponse.getResponseCode());
+        // actually response contains an entire stack trace
+        // assertEquals ("ERROR: Variable $undeclared has not been declared", httpResponse.getResponseMessage());
+        String response = httpResponse.getText();
+        assertTrue (httpResponse.getResponseMessage() + " does not contain expected error message", 
+                httpResponse.getResponseMessage().contains ("Variable $undeclared has not been declared"));
+        assertTrue (response.contains ("Variable $undeclared has not been declared"));
     }
     
     @Test
     public void testParameterMap () throws Exception {
-        String path = (APP_SERVER_PATH + "/src/test/resources/lux/test-params.xqy?p1=A&p2=B&p2=C");
+        String path = (APP_SERVER_PATH + "/test/test-params.xqy?p1=A&p2=B&p2=C");
         String response = httpclient.getResponse(path).getText();
         // This test depends on the order in which keys are retrieved from a java.util.HashMap
         assertEquals ("<http method=\"GET\"><parameters>" +
-        		"<param name=\"p2\"><value>B</value><value>C</value></param>" +
-                "<param name=\"p1\"><value>A</value></param>" +
+        		"<parm name=\"p2\"><value>B</value><value>C</value></parm>" +
+                "<parm name=\"p1\"><value>A</value></parm>" +
         		"</parameters></http>", response.replaceAll("\n\\s*",""));
+    }
+    
+    @Test
+    public void testPlainText () throws Exception {
+        String path = (APP_SERVER_PATH + "/test/test.txt");
+        WebResponse httpResponse = httpclient.getResponse(path);
+        assertEquals ("text/plain", httpResponse.getContentType());
+        String response = httpResponse.getText();
+        // FIXME - somebody is appending an extra newline - is it HttpUnit or us?
+        assertEquals ("This is a test", response.trim());
     }
     
     @BeforeClass
     public static void setup () {
         httpclient = new WebConversation();
+        httpclient.setExceptionsThrownOnErrorStatus(false);
     }
 }
