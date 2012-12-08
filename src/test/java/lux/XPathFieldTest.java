694fe07a5ca8f602ea4e38f9aa8604f7769af3f1
sokolov <sokolov@d2cdb213-60f8-4f1b-b4b2-f6b941818144>
2012-12-08 19:41:44 +0000

    Tests for order by and field-values
    git-svn-id: http://falutin.net/svn/falutin/lux@1291 d2cdb213-60f8-4f1b-b4b2-f6b941818144

--- src/test/java/lux/XPathFieldTest.java
+++ src/test/java/lux/XPathFieldTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.util.Iterator;
 
+import javax.xml.transform.TransformerException;
+
 import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.document.Field.Store;
@@ -35,9 +37,10 @@ public class XPathFieldTest {
     @BeforeClass
     public static void setup () throws Exception {
         XmlIndexer indexer = new XmlIndexer ();
-        indexer.getConfiguration().addField(new XPathField<Integer>("doctype", "name(/*)", null, Store.NO, Type.STRING));
-        indexer.getConfiguration().addField(new XPathField<Integer>("doctype-stored", "name(/*)", null, Store.YES, Type.STRING));
-        
+        indexer.getConfiguration().addField(new XPathField<String>("doctype", "name(/*)", null, Store.NO, Type.STRING));
+        indexer.getConfiguration().addField(new XPathField<String>("doctype-stored", "name(/*)", null, Store.YES, Type.STRING));
+        indexer.getConfiguration().addField(new XPathField<String>("title", "/*/TITLE", null, Store.YES, Type.STRING));
+        // TODO: test integer fields
         dir = new RAMDirectory();
         indexTestSupport = new IndexTestSupport (indexer, dir);
     }
@@ -100,13 +103,75 @@ public class XPathFieldTest {
         assertResultSequence (xquery,  "ACT", "LINE", "PLAY", "SCENE", "SPEECH");
     }
     
-    private void assertResultSequence (String query, String ... results) {
-        XdmResultSet resultSet = eval.evaluate(query); 
+    @Test
+    public void testOrderByEmpty () throws Exception {
+        // get the titles of the first three docs in order by title, using the default "empty least":
+        // these are blank
+        String xquery = "subsequence(for $doc in collection() order by $doc/lux:field-values('title') " +
+            "return string($doc/*/TITLE), 1, 3)";
+        assertResultSequence (xquery, "", "", "");
+
+        // get the first three non-empty titles in order by title, using the default "empty least":
+        // this requires iterating over all the blank titles first, and discarding
+        xquery = "subsequence(for $doc in collection() order by $doc/lux:field-values('title') " +
+        		"return $doc/lux:field-values('title'), 1, 3)";
+        assertResultSequence (xquery, "ACT I", "ACT II", "ACT III");
+
+        // get the first three non-empty titles in order by title, using "empty greatest":
+        // this requires Saxon to perform the sorting since we don't implement empty greatest using Lucene
+        xquery = "subsequence(for $doc in collection() order by $doc/lux:field-values('title') empty greatest " +
+                "return string($doc/*/TITLE), 1, 3)";
+        assertResultSequence (xquery, "ACT I", "ACT II", "ACT III");
+        
+        // Get the first three non-empty titles in reverse order by title, using the default "empty least":
+        xquery = "subsequence(for $doc in collection() order by $doc/lux:field-values('title') descending " +
+        		"return $doc/lux:field-values('title'), 1, 2)";
+        assertResultSequence (xquery, "The Tragedy of Hamlet, Prince of Denmark", 
+                "SCENE VII.  Another room in the castle.");
+    }
+    
+    @Test
+    public void testFieldValuesNoContext () throws Exception {
+        try {
+            assertResultSequence ("lux:field-values('title')", "");
+            assertFalse ("expected exception not thrown", true);
+        } catch (Exception e) {
+            assertTrue (e.getMessage().contains("there is no context defined"));
+        }
+        // FIXME: this should return some kind of error?  The order by expression gets 
+        // removed by the optimizer, but we should make sure it has a context
+        try {
+            assertResultSequence  ("for $doc in collection() order by lux:field-values('title') return $doc");
+            assertFalse ("expected exception not thrown", true);
+        } catch (Exception e) {
+            assertTrue (e.getMessage().contains("there is no context defined"));
+        }
+    }
+    
+    @Test
+    public void testFieldValuesNoField () throws Exception {
+        // no error, just return empty sequence
+        assertResultSequence ("collection()[1]/lux:field-values('bogus')");
+    }
+    
+    @Test
+    public void testAtomizingEmptySequence () throws Exception {
+        String query = "subsequence (for $doc in collection() return string ($doc/*/TITLE), 1, 3)";
+        // should return titles of the first three nodes in document order
+        assertResultSequence (query, "The Tragedy of Hamlet, Prince of Denmark", "", "");
+    }
+    
+    private void assertResultSequence (String query, String ... results) throws TransformerException {
+        XdmResultSet resultSet = eval.evaluate(query);
+        if (resultSet.getErrors() != null) {
+            throw resultSet.getErrors().get(0);
+        }
         Iterator<XdmItem> iter = resultSet.iterator();
         for (String result : results) {
+            assertTrue ("too few results returned", iter.hasNext());
             assertEquals (result, iter.next().getStringValue());
         }
-        assertFalse (iter.hasNext());
+        assertFalse ("query returned extra results", iter.hasNext());
     }
 
     // TODO: test empty greatest / empty least
