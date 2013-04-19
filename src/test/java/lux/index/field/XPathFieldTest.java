97ed0107ffe64973875f59c290f5da7010eca105
Michael Sokolov <sokolov@falutin.net>
2013-04-19 09:14:00 -0400

    work on order by optimization tests

--- src/test/java/lux/index/field/XPathFieldTest.java
+++ src/test/java/lux/index/field/XPathFieldTest.java
@@ -3,6 +3,7 @@ package lux.index.field;
 import static org.junit.Assert.*;
 import lux.Evaluator;
 import lux.IndexTestSupport;
+import lux.QueryStats;
 import lux.XdmResultSet;
 import lux.index.XmlIndexer;
 import lux.index.field.FieldDefinition.Type;
@@ -10,7 +11,9 @@ import net.sf.saxon.s9api.XdmSequenceIterator;
 
 import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.store.RAMDirectory;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -22,7 +25,7 @@ public class XPathFieldTest {
     private static Evaluator eval;
     
     @BeforeClass
-    public static void setup() throws Exception {
+    public static void init() throws Exception {
         XmlIndexer indexer = new XmlIndexer();
         indexer.getConfiguration().addField(new XPathField<Integer>("string-length", "string-length(.)", null, Store.YES, Type.INT));
         indexer.getConfiguration().addField(new XPathField<Integer>("string-length-string", "string-length(.)", null, Store.YES, Type.STRING));
@@ -34,6 +37,11 @@ public class XPathFieldTest {
         indexTestSupport.indexAllElements("lux/reader-test.xml");
         indexTestSupport.reopen(); // commit, make the commit visible
         eval = indexTestSupport.makeEvaluator();
+    }
+    
+    @Before
+    public void setup() {
+    	eval.setQueryStats(new QueryStats());
     }
     
     @Test
@@ -88,6 +96,38 @@ public class XPathFieldTest {
         // 'empty greatest' long
         s1 = getStringResult("for $doc in lux:search('*:*', (), 'string-length-long long empty greatest, name ascending') return (name($doc/*), lux:field-values('string-length-long', $doc))");
         assertEquals("entities 2 entities 3 title 4 token 16 test 95", s1);
+    }
+    
+    @Test
+    public void testSortOptimization () throws Exception {
+        // order by an int field
+    	String s = getStringResult("(for $doc in collection() order by lux:field-values('string-length', $doc) return name($doc/*))[1]");
+        assertEquals ("entities", s);
+        assertEquals (1, eval.getQueryStats().docCount);
+    }
+    
+    @Test
+    public void testSortByConstantFieldValue () throws Exception {
+        // order by a constant value that might look the same - ordering by docid:
+        // What if we produce two sequences, each ordered by a constant involving two different
+        // field-values calls?  They should both be in document order, but the orders will be different...so
+        // we do in fact have to enforce *some* consistent ordering, and docid ordering is the only one that
+        // makes any sense.
+        String name = getStringResult("collection()[1]/ * /name()");
+        eval.getQueryStats().docCount = 0;
+        String s = getStringResult("(for $doc in collection() order by lux:field-values('id', collection()[1]) return name($doc/*))[1]");
+        assertEquals (name, s);
+        // each of 5 documents is retrieved once, so they can be sorted, and then the collection()[1] document is retrieved each time:
+        // - the collection()[1] is not recognized as a constant by the optimizer
+        assertEquals (10, eval.getQueryStats().docCount);
+    }
+    
+    @Test @Ignore
+    public void testSortByXPath() throws Exception {
+        // order by the source path for the field
+    	String s = getStringResult("(for $doc in collection() order by string-length($doc) return name($doc/*))[1]");
+        assertEquals ("entities", s);
+        assertEquals (1, eval.getQueryStats().docCount);
     }
     
     @Test
