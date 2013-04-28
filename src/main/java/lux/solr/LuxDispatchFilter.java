f3813cc5c2944ef77f31af8123a50a5b65a53a58
Michael Sokolov <sokolov@falutin.net>
2013-04-27 20:41:13 -0400

    merge from solr3; setting up webapp for solr dispatch tests

--- src/main/java/lux/solr/LuxDispatchFilter.java
+++ src/main/java/lux/solr/LuxDispatchFilter.java
@@ -1,6 +1,9 @@
 package lux.solr;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,10 +27,36 @@ import javax.servlet.http.HttpServletRequestWrapper;
  * 
  */
 public class LuxDispatchFilter implements Filter {
+    
+    private String baseURI;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        baseURI = filterConfig.getInitParameter("base-uri");
+        if (baseURI == null) {
+            String path;
+            if (File.separatorChar == '\\') {
+                path = "///" + filterConfig.getServletContext().getRealPath("/").replace('\\', '/');
+            } else {
+                path = "//" + filterConfig.getServletContext().getRealPath("/");
+            }
+            // Create a URI since that is supposed to handle quoting of non-URI characters in the path (like spaces)
+            URI uri;
+            try {
+                uri = new URI ("file", path, null);
+            } catch (URISyntaxException e) {
+                throw new ServletException ("Malformed URI for path: " + path, e);
+            }
+            baseURI = uri.toString();
+        }
         
+        // Arrange for initialization of EXPath repository by setting the appropriate system
+        // property, if a path is configured using JNDI:
+
+        String expathRepo = filterConfig.getInitParameter("org.expath.pkg.saxon.repo");
+        if (expathRepo != null) {
+            System.setProperty ("org.expath.pkg.saxon.repo", expathRepo);
+        }
     }
 
     @Override
@@ -36,17 +65,19 @@ public class LuxDispatchFilter implements Filter {
         if( request instanceof HttpServletRequest) {
             HttpServletRequest req = (HttpServletRequest)request;
             String path = req.getServletPath();
-            if (path.contains("/lux/")) {
-                int pathInfoOffset = path.indexOf("/lux/") + 4;
+            int pathInfoOffset = path.indexOf('/', 1);
+            if (pathInfoOffset > 0) {
                 String servletPath = path.substring(0, pathInfoOffset);
                 String pathInfo = path.substring(pathInfoOffset);
                 
                 Request wrapper = new Request(req);
-                wrapper.setServletPath (servletPath);
+                wrapper.setServletPath (servletPath + "/lux");
                 
                 @SuppressWarnings("unchecked")
                 HashMap<String,String[]> params = new HashMap<String, String[]>(req.getParameterMap());
-                params.put ("lux.xquery", new String[] { pathInfo });
+                // load from classpath
+                // TODO: some way of configuring to load from db
+                params.put ("lux.xquery", new String[] { baseURI + pathInfo });
                 wrapper.setParameterMap(params);
                 chain.doFilter(wrapper, response);
                 return;
