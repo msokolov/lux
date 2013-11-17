9a8e256e6b144046ec6e9b501b02dd3adcfd9687
Michael Sokolov <sokolov@falutin.net>
2013-11-16 19:22:40 -0500

    formatting

--- src/main/java/lux/index/analysis/WildcardAnalyzer.java
+++ src/main/java/lux/index/analysis/WildcardAnalyzer.java
@@ -2,12 +2,12 @@ package lux.index.analysis;
 
 import java.io.Reader;
 
-import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
-import org.apache.lucene.analysis.core.KeywordTokenizer;
-import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.KeywordTokenizer;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.util.Version;
 
 /**
@@ -16,7 +16,7 @@ import org.apache.lucene.util.Version;
  * like Solr does with MultiTermAware components when we upgrade to 3.6/4.x.  In NodeQueryBuilder,
  * we have the main Analyzer; this should have a ctor: WildcardAnalyzer(Analyzer base) ...
  */
-public class WildcardAnalyzer extends Analyzer{
+public class WildcardAnalyzer extends Analyzer {
 
     @Override
     protected TokenStreamComponents createComponents(String fieldName, Reader aReader) {
